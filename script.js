4a351463a7b9484d1c5ce18e28815fd500bde945
Georgi Naydenov <georgi3333@hotmail.com>
2020-02-15 10:52:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function createLogo(oddNumber) {
+    let star = '*';
+    let dash = '-';
+    let str = "";
+    let halfLine = ((oddNumber + 1) / 2) - 1;
+
+    for (let col = 0; col <= oddNumber; col++) {
+
+        if (col <= halfLine) {
+            if (col < 1) {
+                str += (dash.repeat(`${oddNumber - col }`) + star.repeat(`${col}`) +
+                    star.repeat(`${oddNumber + col }`) +
+                    dash.repeat(`${oddNumber}`) + star.repeat(col) + star.repeat(`${oddNumber + col}`) +
+                    dash.repeat(oddNumber - col)).repeat(2)  +  '\n';
+                    
+            }
+            if (col >= 1) {
+                str += (dash.repeat(`${oddNumber - col }`) + star.repeat(`${col}`) +
+                    star.repeat(`${oddNumber + col }`) +
+                    dash.repeat(`${(oddNumber - col )  - col}`) + star.repeat(col) + star.repeat(`${oddNumber + col}`) +
+                    dash.repeat(oddNumber - col)).repeat(2) + '\n';
+            }
+        } else if (col > halfLine) {
+
+            if (col == oddNumber) {
+                str += (star.repeat(`${oddNumber}`) +
+                    dash.repeat(`${oddNumber }`) +
+                    star.repeat(`${oddNumber}`) + dash.repeat(oddNumber) + star.repeat(oddNumber)).repeat(2) + '\n';
+            } else {
+                str += (dash.repeat(`${oddNumber - col }`) + star.repeat(`${oddNumber}`) +
+                    dash.repeat(`${ col - (oddNumber  - col)  }`) +
+                    star.repeat(`${ (oddNumber - col) * 2 + oddNumber }`)  + dash.repeat(col - (oddNumber - col)) + star.repeat(oddNumber) +
+                    dash.repeat(oddNumber - col)).repeat(2) + '\n';
+            }
+        }
+    }
+    return str ;
+}
+
+console.log(createLogo(3))
+//console.log(createLogo(5))
+//console.log(createLogo(7))
